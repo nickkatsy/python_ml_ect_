bac07d3a25ecd5cd7d5770d932034af1cdf6d1c0
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-10-12 07:17:25 -0400

    Update quick_housing.py

--- quick_housing.py
+++ quick_housing.py
@@ -20,22 +20,24 @@ df = df.drop('obsn_num',axis=1)
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def basic_subplots(dataframe):
+  plt____,axs = plt.subplots(2,2,figsize=(12,6))
+  sns.scatterplot(df,x='income',y='house_price',ax=axs[0,0])
+  sns.barplot(df,x='in_cali',ax=axs[0,1])
+  sns.violinplot(df['income'],ax=axs[1,0])
+  sns.distplot(df['earthquake'],ax=axs[1,1])
+  plt.show()
 
-plt____,axs = plt.subplots(2,2,figsize=(12,6))
-plt_1 = sns.scatterplot(df,x='income',y='house_price',ax=axs[0,0])
-plt_2 = sns.barplot(df,x='in_cali',ax=axs[0,1])
-plt_3 = sns.violinplot(df['income'],ax=axs[1,0])
-plt_4 = sns.distplot(df[['earthquake']],ax=axs[1,1])
 
+basic_subplots(df)
 
-
-
-# Just a simple model for stuuuuuuuuf, in_cali for now I do not know I do no know atm
+# Selection
 
 X = df.drop('in_cali',axis=1)
 y = df[['in_cali']]
 
 
+# scaling the features
 
 from sklearn.preprocessing import StandardScaler
 
@@ -44,7 +46,7 @@ sc = StandardScaler()
 X_scaler = sc.fit_transform(X)
 y_scaler = sc.fit_transform(y)
 
-
+# Creating Classifier
 from sklearn.linear_model import LogisticRegression
 clf = LogisticRegression().fit(X_scaler,y_scaler)
 
@@ -52,7 +54,7 @@ clf_pred = clf.predict(X_scaler)
 
 clf_pred_prob = clf.predict_proba(X_scaler)[::,1]
 
-
+# 5-Fold CV
 from sklearn.model_selection import cross_val_score
 
 cv = cross_val_score(clf,X_scaler,y_scaler,cv=5,scoring='roc_auc').mean()
