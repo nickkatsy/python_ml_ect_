2916534b5667f5c23bb1ab0d6e0fe46292c03e7d
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-10-12 07:00:37 -0400

    Update credit_risk.py

--- credit_risk.py
+++ credit_risk.py
@@ -20,19 +20,20 @@ import matplotlib.pyplot as plt
 
 sns.heatmap(df.corr(),annot=True)
 
+def descriptive_subplots(dataframe):
+    plt_,axs = plt.subplots(3,3,figsize=(10,6))
+    sns.countplot(df,x='Home',ax=axs[0,0])
+    sns.countplot(df,x='Status',ax=axs[0,1])
+    sns.scatterplot(df,x='Age',y='Income',ax=axs[0,2])
+    sns.violinplot(df[['Status']],ax=axs[1,0])
+    sns.countplot(df,x='Default',ax=axs[1,1])
+    sns.kdeplot(df,x='Age',ax=axs[1,2])
+    sns.countplot(df,x='Intent',ax=axs[2,0])
+    sns.scatterplot(df[['Income']],ax=axs[2,1])
+    sns.countplot(df,x='Status',ax=axs[2,2])
+    plt.show()
 
-plt_,axs = plt.subplots(3,3,figsize=(10,6))
-plt_1 = sns.countplot(df,x='Home',ax=axs[0,0])
-plt_2 = sns.countplot(df,x='Status',ax=axs[0,1])
-plt_3 = sns.scatterplot(df,x='Age',y='Income',ax=axs[0,2])
-plt_4 = sns.violinplot(df[['Status']],ax=axs[1,0])
-plt_5 = sns.countplot(df,x='Default',ax=axs[1,1])
-plt_6 = sns.kdeplot(df,x='Age',ax=axs[1,2])
-plt_7 = sns.countplot(df,x='Intent',ax=axs[2,0])
-plt_8 = sns.scatterplot(df[['Income']],ax=axs[2,1])
-plt_9 = sns.countplot(df,x='Status',ax=axs[2,2])
-
-
+descriptive_subplots(df)
 
 df = df.drop('Id',axis=1)
 
@@ -74,16 +75,17 @@ ct.fit_transform(X)
 
 from sklearn.pipeline import make_pipeline
 
-
+# Pipeline for logistic regression
 clf_pipe = make_pipeline(ct,clf).fit(X_train,y_train)
 clf_pred = clf_pipe.predict(X_test)
 clf_pred_prob = clf_pipe.predict_proba(X_test)[::,1]
 
-
+# Making a pipeline for Random Forrest Classifier
 rfc_pipe = make_pipeline(ct,rfc).fit(X_train,y_train)
 rfc_pred = rfc_pipe.predict(X_test)
 rfc_pred_prob = rfc_pipe.predict_proba(X_test)[::,1]
 
+# Results
 from sklearn.metrics import accuracy_score,roc_auc_score,roc_curve
 
 acc_clf = accuracy_score(y_test, clf_pred)
@@ -98,6 +100,7 @@ print('Random Forest Accuracy= ',acc_rfc*100)
 roc_rfc = roc_auc_score(y_test, rfc_pred_prob)
 print('roc for Random Forest= ',roc_rfc*100)
 
+# plot of ROC/AUC from best performing model(rfc)
 fpr, tpr, _ = roc_curve(y_test,  rfc_pred_prob)
 
 plt.plot(fpr,tpr)
@@ -105,3 +108,4 @@ plt.title('Random Forest')
 plt.ylabel('True Positive Rate')
 plt.xlabel('False Positive Rate')
 plt.show()
+
