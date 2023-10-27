1d05eb3f80db7ea83d48d35b8c0d39174d05e2c4
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-10-27 18:01:51 -0400

    Update Default_model.py

--- Default_model.py
+++ Default_model.py
@@ -10,6 +10,9 @@ df.isnull().sum()
 df.nunique()
 df.corr()
 
+
+df['default'] = 1 - df['default']
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -44,48 +47,42 @@ rating_subplots(df)
 X = df.drop('default', axis=1)
 y = df['default']
 
-from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=42)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit_transform(X)
 
 from sklearn.linear_model import LogisticRegression
 
 
-clf = LogisticRegression().fit(X_train, y_train)
-clf_pred = clf.predict(X_test)
-clf_pred_prob = clf.predict_proba(X_test)[::,1]
+clf = LogisticRegression().fit(X,y)
+clf_pred = clf.predict(X)
+clf_pred_prob = clf.predict_proba(X)[::,1]
 
 from sklearn.ensemble import RandomForestClassifier
 
 # Random Forest Model
-rfc = RandomForestClassifier().fit(X_train, y_train)
-rfc_pred = rfc.predict(X_test)
-rfc_pred_prob = rfc.predict_proba(X_test)[::,1]
+rfc = RandomForestClassifier().fit(X,y)
+rfc_pred = rfc.predict(X)
+rfc_pred_prob = rfc.predict_proba(X)[::,1]
 
-from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score
+
+
+from sklearn.metrics import accuracy_score
 
 # Model Performance
-acc_logistic_regression = accuracy_score(y_test, clf_pred)
-roc_clf = roc_auc_score(y_test, clf_pred_prob)
-acc_rfc = accuracy_score(y_test, rfc_pred)
-roc_rfc = roc_auc_score(y_test, clf_pred_prob)
+acc_logistic_regression = accuracy_score(y, clf_pred)
+print(f' the accuracy using Logistic Regression: {acc_logistic_regression*100}')
 
 
-# results
-print('Logistic Regression Accuracy:', acc_logistic_regression * 100)
-print('Logistic Regression ROC AUC:', roc_clf * 100)
-print('Random Forest Accuracy:', roc_rfc * 100)
-print('Random Forest ROC AUC:', roc_rfc * 100)
+acc_rfc = accuracy_score(y, rfc_pred)
+print(f'the accuracy of random forest for this model: {acc_rfc*100}')
 
-fpr_clf, tpr_clf, _ = roc_curve(y_test, clf_pred_prob)
-fpr_rfc, tpr_rfc, _ = roc_curve(y_test, rfc_pred_prob)
 
-# ROC for RFC and Logistic Regression and Random Forest
-plt.figure(figsize=(8, 6))
-plt.plot(fpr_clf, tpr_clf, label='Logistic Regression')
-plt.plot(fpr_rfc, tpr_rfc, label='Random Forest')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend()
-plt.show()
+from sklearn.model_selection import cross_val_predict
+clf_pred_prob_cv = cross_val_predict(clf, X, y, cv=5, method='predict_proba').max()
+rfc_pred_prob_cv = cross_val_predict(rfc, X, y, cv=5, method='predict_proba').max()
+
+
+print(f'ROC using 5-fold cross-validation with Logistic Regression: {clf_pred_prob_cv*100}')
+print(f'ROC using 5-fold cross-validation with Random Forest: {rfc_pred_prob_cv*100}')
