0dc5fc429978805c22e02a783c95b41c4ecf3c9b
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-11-11 13:50:27 -0500

    Update heart_attack_edit.py

--- heart_attack_edit.py
+++ heart_attack_edit.py
@@ -2,8 +2,6 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-
-
 df = pd.read_csv('https://raw.githubusercontent.com/nickkas/python_ml_ect_/master/heart.csv')
 
 df.info()
@@ -23,16 +21,35 @@ df.corr()
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-plt_,ax1 = plt.subplots(3,3,figsize=(12,5))
-plt_1 = sns.histplot(df[['age']],ax=ax1[0,0])
-plt_2 = sns.violinplot(df[['cp']],ax=ax1[0,1])
-plt_3 = sns.histplot(df[['trestbps']],ax=ax1[0,2])
-plt_4 = sns.histplot(df[['fbs']],ax=ax1[1,0])
-plt_5 = sns.histplot(df[['sex']],ax=ax1[1,1])
-plt_6 = sns.histplot(df['exang'],ax=ax1[1,2])
-plt_7 = sns.histplot(df[['heart_attack']],ax=ax1[2,0])
-plt_8 = sns.histplot(df[['chol']],ax=ax1[2,1])
-plt_9 = sns.kdeplot(df[['oldpeak']],ax=ax1[2,2])
+def subplots(df):
+    plt_,ax1 = plt.subplots(3,3,figsize=(12,5))
+    sns.histplot(df[['age']],ax=ax1[0,0])
+    sns.violinplot(df[['cp']],ax=ax1[0,1])  
+    sns.histplot(df[['trestbps']],ax=ax1[0,2])
+    sns.histplot(df[['fbs']],ax=ax1[1,0])
+    sns.histplot(df[['sex']],ax=ax1[1,1])
+    sns.histplot(df['exang'],ax=ax1[1,2])
+    sns.histplot(df[['heart_attack']],ax=ax1[2,0])
+    sns.histplot(df[['chol']],ax=ax1[2,1])
+    sns.kdeplot(df[['oldpeak']],ax=ax1[2,2])
+    plt.show()
+
+
+
+subplots(df)
+
+
+import statsmodels.api as sm
+
+model_sex = sm.GLM(exog=sm.add_constant(df[['sex']]),endog=df[['heart_attack']]).fit()
+print(model_sex.summary())
+
+
+model_age = sm.OLS(exog=sm.add_constant(df['age']),endog=df[['heart_attack']]).fit()
+print(model_age.summary())
+
+model_chol = sm.OLS(exog=sm.add_constant(df[['chol']]),endog=df[['heart_attack']]).fit()
+print(model_chol.summary())
 
 
 
@@ -71,7 +88,7 @@ rf_pred_prob = rf.predict_proba(X_test)[::,1]
 
 
 
-from sklearn.metrics import roc_auc_score,accuracy_score
+from sklearn.metrics import roc_auc_score,accuracy_score,roc_curve
 
 #Logistic Regression
 
@@ -90,3 +107,15 @@ print('Random Forest accuracy= ',rff_acc*100)
 
 rff_roc = roc_auc_score(y_test,rf_pred_prob)
 print('Random Forest roc= ',rff_roc*100)
+
+
+def roc_rfc(y_true,rf_pred_prob):
+    fpr,tpr, _ = roc_curve(y_test,rf_pred_prob)
+    plt.plot(fpr,tpr)
+    plt.title('ROC curve for random forest')
+    plt.ylabel('True positive rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+    
+
+roc_rfc(y_test, rf_pred_prob)
