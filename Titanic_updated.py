a5b9be6b89b3de4c46850c4003bdbe818ee41854
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-11-11 20:48:25 -0500

    Update Titanic_updated.py

--- Titanic_updated.py
+++ Titanic_updated.py
@@ -26,22 +26,34 @@ copy.dropna()
 copy['Sex'] = copy['Sex'].map({'female':0,'male':1})
 
 
-plt_,ax1 = plt.subplots(2,3,figsize=(10,6))
-plt_1 = sns.kdeplot(copy[['Fare']],ax=ax1[0,0])
-plt_2 = sns.histplot(copy[['Survived']],ax=ax1[0,1])
-plt_3 = sns.violinplot(copy[['Pclass']],ax=ax1[0,2])
-plt_4 = sns.boxplot(copy[['SibSp']],ax=ax1[1,0])
-plt_5 = sns.histplot(copy[['Age']],ax=ax1[1,1])
-plt_6 = sns.histplot(copy[['Sex']],ax=ax1[1,2])
+def misc(copy):
+    plt_,ax1 = plt.subplots(2,3,figsize=(10,6))
+    sns.kdeplot(copy[['Fare']],ax=ax1[0,0])
+    sns.histplot(copy[['Survived']],ax=ax1[0,1])
+    sns.violinplot(copy[['Pclass']],ax=ax1[0,2])
+    sns.boxplot(copy[['SibSp']],ax=ax1[1,0])
+    sns.histplot(copy[['Age']],ax=ax1[1,1])
+    sns.histplot(copy[['Sex']],ax=ax1[1,2])
+    plt.show()
 
 
-plt_2, ax2 = plt.subplots(2,2,figsize=(10,6))
-plt2_ = sns.barplot(copy,x='Sex',y='Survived',ax=ax2[0,0])
-plt2_1 = sns.boxplot(copy,x='Survived',y='Age',ax=ax2[0,1])
-plt2_2 = sns.histplot(copy[['Age']],ax=ax2[1,0])
-plt2_3 = sns.barplot(copy,x='Sex',y='Fare',ax=ax2[1,1])
+misc(copy)
 
-del(copy)
+
+
+def more_subs(copy):
+    plt_2,ax2 = plt.subplots(2,2,figsize=(10,6))
+    sns.barplot(copy,x='Sex',y='Survived',ax=ax2[0,0])
+    sns.boxplot(copy,x='Survived',y='Age',ax=ax2[0,1])
+    sns.histplot(copy[['Age']],ax=ax2[1,0])
+    sns.barplot(copy,x='Sex',y='Fare',ax=ax2[1,1])
+    plt.show()
+
+
+
+more_subs(copy)
+
+
 
 X = df.drop('Survived',axis=1)
 y = df[['Survived']]
@@ -103,7 +115,7 @@ pipe_tree = make_pipeline(ct,tree_clf).fit(X_train,y_train)
 pred_tree = pipe_tree.predict(X_test)
 pred_prob_tree = pipe_tree.predict_proba(X_test)[::,1]
 
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
 
 #logistic regression results
 
@@ -126,3 +138,15 @@ print('roc_auc for knn classification',roc_auc_score(y_test,knn_pred_prob))
 # Decision Tree Classfication results
 print('Accuracy for decsion tree classifier= ',accuracy_score(y_test,pred_tree))
 print('ROC for tree classifier',roc_auc_score(y_test, pred_prob_tree))
+
+
+def logistic_roc(y_test,pred_prob_clf):
+    fpr,tpr, _ = roc_curve(y_test,pred_prob_clf)
+    plt.plot(fpr,tpr)
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.title('The ROC curve from the Logistic Regression Model')
+    plt.show()
+    
+
+logistic_roc(y_test, pred_prob_clf)
