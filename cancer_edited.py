3eab06ae68b4c9c77c719fe0c74c8fe6b434dcec
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-12-16 16:06:34 -0500

    Update cancer_edited.py

--- cancer_edited.py
+++ cancer_edited.py
@@ -19,13 +19,13 @@ df.shape
 df = df.drop(['Unnamed: 32','id'],axis=1)
 
 df.corr()
-
+df['diagnosis'].value_counts()
 
 
 df['diagnosis'] = pd.get_dummies(df.diagnosis,prefix='diagnosis').iloc[:,0:1]
 
 def subplots(df):
-    his,ax1 = plt.subplots(4,3,figsize=(10,6))
+    his,ax1 = plt.subplots(4,3,figsize=(12,6))
     sns.kdeplot(df,x='diagnosis',ax=ax1[0,0])
     sns.histplot(df,x='radius_mean',ax=ax1[0,1])
     sns.histplot(df,x='texture_mean',ax=ax1[0,2])
@@ -54,15 +54,20 @@ y = df[['diagnosis']]
 y.value_counts(normalize=True)
 
 
-from sklearn.preprocessing import StandardScaler
 
-sc = StandardScaler()
-sc.fit_transform(X)
 
 
 from sklearn.model_selection import train_test_split
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
+
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train_scaled = sc.fit_transform(X_train)
+X_test_scaled = sc.transform(X_test)
+
 
 
 from sklearn.linear_model import LogisticRegression
@@ -71,32 +76,28 @@ from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
 
 
-clf = LogisticRegression().fit(X_train,y_train)
-pred_clf = clf.predict(X_test)
-pred_prob_clf = clf.predict_proba(X_test)[::,1]
+clf = LogisticRegression().fit(X_train_scaled,y_train)
+pred_clf = clf.predict(X_test_scaled)
+pred_prob_clf = clf.predict_proba(X_test_scaled)[::,1]
 
 
-tree = DecisionTreeClassifier().fit(X_train,y_train)
-tree_pred = tree.predict(X_test)
-tree_pred_prob = tree.predict_proba(X_test)[::,1]
+tree = DecisionTreeClassifier().fit(X_train_scaled,y_train)
+tree_pred = tree.predict(X_test_scaled)
+tree_pred_prob = tree.predict_proba(X_test_scaled)[::,1]
 
-rfc = RandomForestClassifier().fit(X_train,y_train)
-rfc_pred = rfc.predict(X_test)
-rfc_pred_prob = rfc.predict_proba(X_test)[::,1]
-
-
-nb = GaussianNB().fit(X_train,y_train)
-nb_pred = nb.predict(X_test)
-nb_pred_prob = nb.predict_proba(X_test)[::,1]
+rfc = RandomForestClassifier().fit(X_train_scaled,y_train)
+rfc_pred = rfc.predict(X_test_scaled)
+rfc_pred_prob = rfc.predict_proba(X_test_scaled)[::,1]
 
 
-GBC = GradientBoostingClassifier().fit(X_train, y_train)
-GBC_pred = GBC.predict(X_test)
-GBC_pred_prob = GBC.predict_proba(X_test)[::,1]
+nb = GaussianNB().fit(X_train_scaled,y_train)
+nb_pred = nb.predict(X_test_scaled)
+nb_pred_prob = nb.predict_proba(X_test_scaled)[::,1]
 
 
-
-
+GBC = GradientBoostingClassifier().fit(X_train_scaled, y_train)
+GBC_pred = GBC.predict(X_test_scaled)
+GBC_pred_prob = GBC.predict_proba(X_test_scaled)[::,1]
 
 
 
@@ -115,4 +116,3 @@ evaluate_model('Random Forest', y_test,rfc_pred,rfc_pred_prob)
 evaluate_model('Naive Bayes', y_test,nb_pred,nb_pred_prob)
 evaluate_model('Gradient Boosting',y_test,GBC_pred,GBC_pred_prob)
 evaluate_model('Decision Trees', y_test,tree_pred, tree_pred_prob)
-
