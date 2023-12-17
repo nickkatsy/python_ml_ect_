a2a6c81f1079581ac03fde9598e42e517c0819f7
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-12-17 10:30:20 -0500

    Update black_and_yellow.py

--- black_and_yellow.py
+++ black_and_yellow.py
@@ -2,25 +2,18 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-
 df = pd.read_csv('https://raw.githubusercontent.com/JoshuaEubanksUCF/Black_and_Gold_Analytics/main/Student_Basket_Data/BGA_basketball_data.csv')
 
 df.info()
 df.isna().sum()
-
 
 print(df['Made'].describe())
 
 df['Major'].value_counts()
 df['Major'] = df['Major'].str.replace(',', '-', regex=True) 
 
-
-df.isna().sum()
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
 sns.heatmap(df.corr(), annot=True)
 
 
@@ -34,8 +27,6 @@ def desc_subplots(df):
 
 
 desc_subplots(df)
-
-
 
 df['Major'] = pd.factorize(df['Major'])[0]
 df['Name'] = pd.factorize(df['Name'])[0]
@@ -99,9 +90,6 @@ evaluate_model('Gradient Boosting',y_test,gbc_pred,gbc_pred_prob)
 
 
 
-
-
-
 # ROC curves plotted based on the results of the models
 
 def roc_curve_plot(y_test, y_pred_prob, model_name):
@@ -117,3 +105,36 @@ roc_curve_plot(y_test,gbc_pred_prob,'Gradient Boosting')
 roc_curve_plot(y_test,rfc_pred_prob,'Random Forest')
 plt.legend()
 plt.show()
+
+
+
+# using cross-validation and polynomial features
+
+from sklearn.preprocessing import PolynomialFeatures
+
+from sklearn.model_selection import cross_val_score
+
+
+def evaluate_cv_model(model,X,y,cv=10):
+    poly = PolynomialFeatures(2)
+    X_poly = poly.fit_transform(X)
+
+    cv_scores = cross_val_score(model,X_poly,y,cv=cv,scoring='roc_auc')
+    max_roc_auc = cv_scores.max()
+
+
+    print(f'{model.__class__.__name__} Cross-Validated ROC-AUC Scores:')
+    print(cv_scores)
+    print(f'Max ROC-AUC: {max_roc_auc * 100:.2f}%')
+
+    return max_roc_auc
+
+
+
+
+clf_lr = LogisticRegression()
+clf_rf = RandomForestClassifier()
+clf_gb = GradientBoostingClassifier()
+roc_auc_lr = evaluate_cv_model(clf_lr,X,y)
+roc_auc_rf = evaluate_cv_model(clf_rf,X,y)
+roc_auc_gb = evaluate_cv_model(clf_gb,X,y)
