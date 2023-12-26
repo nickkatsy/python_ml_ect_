8ba47ede99a46718905165c4928e7198e2d48a3a
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-12-25 19:20:38 -0500

    Update household_income_revised.py

--- household_income_revised.py
+++ household_income_revised.py
@@ -2,7 +2,7 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv('C:/ml/python/data/income.csv',delimiter=',')
+df = pd.read_csv('https://raw.githubusercontent.com/nickkatsy/python_ml_ect_/master/income.csv')
 
 
 df.info()
