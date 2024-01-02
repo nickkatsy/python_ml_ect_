c32ed22ba200a96e899709a695ecacfd3e341d4f
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-01-01 23:29:35 -0500

    Update household_income_revised.py

--- household_income_revised.py
+++ household_income_revised.py
@@ -81,7 +81,7 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=
 
 
 from sklearn.preprocessing import OneHotEncoder
-ohe = OneHotEncoder(sparse=False)
+ohe = OneHotEncoder(sparse_output=False)
 
 from sklearn.preprocessing import StandardScaler
 
@@ -97,11 +97,15 @@ ct = make_column_transformer(
 
 ct.fit_transform(X)
 
+from sklearn.pipeline import make_pipeline
 
 
 from sklearn.linear_model import LogisticRegression
 lr = LogisticRegression()
-from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
+
+
+
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,BaggingClassifier
 rfc = RandomForestClassifier()
 gbc = GradientBoostingClassifier()
 
@@ -117,107 +121,93 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 lda = LinearDiscriminantAnalysis()
 
 
-from sklearn.pipeline import make_pipeline
+BC = BaggingClassifier()
 
-lr_pipe = make_pipeline(ct,lr).fit(X_train,y_train)
-lr_pred = lr_pipe.predict(X_test)
-lr_pred_prob = lr_pipe.predict_proba(X_test)[::,1]
-
-rfc_pipe = make_pipeline(ct,rfc).fit(X_train,y_train)
-rfc_pred = rfc_pipe.predict(X_test)
-rfc_pred_prob = rfc_pipe.predict_proba(X_test)[::,1]
-
-gbc_pipe = make_pipeline(ct,gbc).fit(X_train,y_train)
-gbc_pred = gbc_pipe.predict(X_test)
-gbc_pred_prob = gbc_pipe.predict_proba(X_test)[::,1]
-
-
-nb_pipe = make_pipeline(ct,lda).fit(X_train,y_train)
-nb_pred = nb_pipe.predict(X_test)
-nb_pred_prob = nb_pipe.predict_proba(X_test)[::,1]
-
-
-lda_pipe = make_pipeline(ct,lda).fit(X_train,y_train)
-lda_pred = lda_pipe.predict(X_test)
-lda_pred_prob = lda_pipe.predict_proba(X_test)[::,1]
-
-
-tree_pipe = make_pipeline(ct,trees).fit(X_train,y_train)
-tree_pred = tree_pipe.predict(X_test)
-tree_pred_prob = tree_pipe.predict_proba(X_test)[::,1]
 
 
 from sklearn.neighbors import KNeighborsClassifier
 
 knn = KNeighborsClassifier(n_neighbors=10)
-knn_pipe = make_pipeline(ct,knn).fit(X_train,y_train)
-knn_pred = knn_pipe.predict(X_test)
-knn_pred_prob = knn_pipe.predict_proba(X_test)[::,1]
 
 
 
-from sklearn.metrics import roc_auc_score,accuracy_score,roc_curve
+from sklearn.metrics import roc_auc_score,accuracy_score,roc_curve,f1_score
 
-def evaluate_model(y_test,y_pred,y_pred_prob,model_name):
-    acc = accuracy_score(y_test, y_pred)
-    roc = roc_auc_score(y_test, y_pred_prob)
-    print(f'{model_name} --Accuracy Score-- {acc*100:.2f}%, --ROC-- {roc*100:.2f}%')
+def evaluate_model(model,X_train,X_test,y_train,y_test):
+    pipe = make_pipeline(ct,model).fit(X_train,y_train)
+    pred = pipe.predict(X_test)
+    pred_prob = pipe.predict_proba(X_test)[:,1]
+    acc = accuracy_score(y_test, pred)
+    f1 = f1_score(y_test, pred)
+    roc = roc_auc_score(y_test,pred_prob)
+    print(f'{model.__class__.__name__}, --Accuracy Score-- {acc*100:.2f}%; --ROC-- {roc*100:.2f}%; --F1-- {f1*100:.2f}%')
+    return pred,pred_prob
+
+
+lr_pred,lr_pred_prob = evaluate_model(lr, X_train, X_test, y_train, y_test)
+rfc_pred,rfc_pred_prob = evaluate_model(rfc, X_train, X_test, y_train, y_test)
+gbc_pred,gbc_pred_prob = evaluate_model(gbc, X_train, X_test, y_train, y_test)
+trees_pred,trees_pred_prob = evaluate_model(trees, X_train, X_test, y_train, y_test)
+nb_pred,nb_pred_prob = evaluate_model(nb, X_train, X_test, y_train, y_test)
+lda_pred,lda_pred_prob = evaluate_model(lda, X_train, X_test, y_train, y_test)
+BC_pred,BC_pred_prob = evaluate_model(BC, X_train, X_test, y_train, y_test)
+knn_pred,knn_pred_prob = evaluate_model(knn, X_train, X_test, y_train, y_test)
 
 
 
-evaluate_model(y_test, lr_pred, lr_pred_prob, 'Logistic Regression')
-evaluate_model(y_test, rfc_pred, rfc_pred_prob, 'Random Forest')
-evaluate_model(y_test, gbc_pred, gbc_pred_prob, 'Gradient Boost')
-evaluate_model(y_test, nb_pred, nb_pred_prob, 'Naive Bayes')
-evaluate_model(y_test, lda_pred, lda_pred_prob, 'LDA')
-evaluate_model(y_test, knn_pred, knn_pred_prob, 'K-NearestNeighbors')
-evaluate_model(y_test, tree_pred, tree_pred_prob, 'Decision Trees')
 
-def ROC(y_test,y_pred_prob,model_name):
+def ROC(y_test,y_pred_prob,model):
     fpr,tpr, _ = roc_curve(y_test,y_pred_prob)
-    plt.plot(fpr,tpr,label=model_name)
+    plt.plot(fpr,tpr,label=model.__class__.__name__)
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    
+    plt.title('ROC CURVE')
 
 
-ROC(y_test, lr_pred_prob,'Logistic Regression')
-ROC(y_test,gbc_pred_prob,'Gradient Boosting')
-ROC(y_test, rfc_pred_prob, 'Random Forest')
-ROC(y_test,lda_pred_prob,'LDA')
-ROC(y_test, nb_pred_prob, 'Naive Bayes')
-ROC(y_test, knn_pred_prob, 'K-Nearest Neighbors')
-ROC(y_test,tree_pred_prob,'Decision Trees')
+ROC(y_test, lr_pred_prob,lr)
+ROC(y_test,gbc_pred_prob,gbc)
+ROC(y_test, rfc_pred_prob, rfc)
+ROC(y_test,lda_pred_prob,lda)
+ROC(y_test, nb_pred_prob, nb)
+ROC(y_test, knn_pred_prob, knn)
+ROC(y_test,trees_pred_prob,trees)
+ROC(y_test,BC_pred_prob,BC)
+ROC(y_test,knn_pred_prob,knn)
 plt.legend()
 plt.show()
 
-# GridSearch on all parameters
-# WARNING: This will take a long time
+
+#GridSearch
+
 from sklearn.model_selection import RandomizedSearchCV
 
 import numpy as np
-
 
 lr_random_param_grid = {
     'logisticregression__C': np.logspace(-4,4,9),
     'logisticregression__penalty': ['l1','l2']
 }
 
-
 lr_random_search = RandomizedSearchCV(
-    lr_pipe,lr_random_param_grid,n_iter=10,scoring='roc_auc',cv=5,random_state=42
+    make_pipeline(ct,lr), 
+    lr_random_param_grid,
+    n_iter=10,
+    scoring='roc_auc',
+    cv=5,
+    random_state=42
 ).fit(X_train, y_train)
+
 
 # Best Logistic Regression model
 best_lr_estimator = lr_random_search.best_estimator_
 lr_random_pred = best_lr_estimator.predict(X_test)
-lr_random_pred_prob = best_lr_estimator.predict_proba(X_test)[::,1]
+lr_random_pred_prob = best_lr_estimator.predict_proba(X_test)[:,1]
 print(f'Logistic Regression - Best Parameters: {lr_random_search.best_params_}')
 print(f'Logistic Regression - Best ROC-AUC Score: {lr_random_search.best_score_ * 100:.2f}%')
 
 
 # Random forest Random Grid Search
-rfc_random_param_grid = {
+rfc_param_grid = {
     'randomforestclassifier__n_estimators': [50,100,200],
     'randomforestclassifier__max_depth': [None,10,20],
     'randomforestclassifier__min_samples_split': [2,5,10],
@@ -225,18 +215,21 @@ rfc_random_param_grid = {
 }
 
 rfc_random_search = RandomizedSearchCV(
-    rfc_pipe, rfc_random_param_grid,n_iter=10,scoring='roc_auc',cv=5,random_state=42
-).fit(X_train,y_train)
-
+    make_pipeline(ct,rfc),
+    rfc_param_grid,
+    n_iter=10,
+    scoring='roc_auc',
+    cv=5, 
+    random_state=42
+).fit(X_train, y_train)
 
 best_rfc_estimator = rfc_random_search.best_estimator_
 rfc_random_pred = best_rfc_estimator.predict(X_test)
-rfc_random_pred_prob = best_rfc_estimator.predict_proba(X_test)[::,1]
+rfc_random_pred_prob = best_rfc_estimator.predict_proba(X_test)[:, 1]
 print(f'Random Forest - Best Parameters: {rfc_random_search.best_params_}')
-print(f'Random Forest - Best ROC-AUC Score: {rfc_random_search.best_score_ * 100:.2f}%')
+print(f'Random Forest - Best ROC-AUC Score: {rfc_random_search.best_score_ * 100:.2f}')
 
 
-#Gradientboost gridsearch
 
 gbc_random_param_grid = {
     'gradientboostingclassifier__n_estimators': [50,100,200],
@@ -246,7 +239,12 @@ gbc_random_param_grid = {
 }
 
 gbc_random_search = RandomizedSearchCV(
-    gbc_pipe,gbc_random_param_grid,n_iter=10,scoring='roc_auc',cv=5,random_state=42
+    make_pipeline(ct,gbc),
+    gbc_random_param_grid,
+    n_iter=10,
+    scoring='roc_auc',
+    cv=5,
+    random_state=42
 ).fit(X_train,y_train)
 
 
@@ -258,7 +256,8 @@ print(f'Gradient Boosting - Best Parameters: {gbc_random_search.best_params_}')
 print(f'Gradient Boosting - Best ROC-AUC Score: {gbc_random_search.best_score_ * 100:.2f}%')
 
 
-# KNN Random Grid Search
+
+# K-Nearest Neighbors RandomGridSearch
 
 knn_param_grid = {
     'kneighborsclassifier__n_neighbors': [3,5,7,10],
@@ -266,7 +265,11 @@ knn_param_grid = {
     'kneighborsclassifier__p': [1,2]
 }
 
-knn_grid_search = RandomizedSearchCV(knn_pipe,knn_param_grid,scoring='roc_auc',cv=5).fit(X_train, y_train)
+knn_grid_search = RandomizedSearchCV(
+    make_pipeline(ct,knn),
+    knn_param_grid,
+    scoring='roc_auc',
+    cv=5).fit(X_train, y_train)
 
 
 best_knn_estimator = knn_grid_search.best_estimator_
@@ -275,7 +278,8 @@ knn_grid_pred_prob = best_knn_estimator.predict_proba(X_test)[::,1]
 print(f'K-Nearest Neighbors - Best Parameters: {knn_grid_search.best_params_}')
 print(f'K-Nearest Neighbors - Best ROC-AUC Score: {knn_grid_search.best_score_ * 100:.2f}%')
 
-#Trees
+
+# Decision Tree Randomized Grid Search
 
 dt_param_dist = {
     'decisiontreeclassifier__criterion': ['gini','entropy'],
@@ -285,7 +289,12 @@ dt_param_dist = {
 }
 
 dt_rand_search = RandomizedSearchCV(
-    tree_pipe,dt_param_dist,n_iter=10,scoring='roc_auc',cv=5,random_state=42
+    make_pipeline(ct,trees),
+    dt_param_dist,
+    n_iter=10,
+    scoring='roc_auc',
+    cv=5,
+    random_state=42
 ).fit(X_train,y_train)
 
 
@@ -295,7 +304,9 @@ dt_rand_pred_prob = best_dt_estimator.predict_proba(X_test)[::,1]
 print(f'Decision Trees - Best Parameters: {dt_rand_search.best_params_}')
 print(f'Decision Trees - Best ROC-AUC Score: {dt_rand_search.best_score_ * 100:.2f}%')
 
-#LDA
+
+
+#LDA Ranomized GridSearch
 
 lda_param_dist = {
     'lineardiscriminantanalysis__solver': ['svd','lsqr','eigen'],
@@ -303,9 +314,13 @@ lda_param_dist = {
 }
 
 lda_rand_search = RandomizedSearchCV(
-    lda_pipe,lda_param_dist,n_iter=10,scoring='roc_auc',cv=5,random_state=42
-)
-lda_rand_search.fit(X_train, y_train)
+    make_pipeline(ct,lda),
+    lda_param_dist,
+    n_iter=10,
+    scoring='roc_auc',
+    cv=5,
+    random_state=42
+).fit(X_train, y_train)
 
 best_lda_estimator = lda_rand_search.best_estimator_
 lda_rand_pred = best_lda_estimator.predict(X_test)
@@ -314,12 +329,24 @@ print(f'Linear Discriminant Analysis - Best Parameters: {lda_rand_search.best_pa
 print(f'Linear Discriminant Analysis - Best ROC-AUC Score: {lda_rand_search.best_score_ * 100:.2f}%')
 
 
-# This took very,very long.
-# ROC Curves for Hyper-Parameters
-ROC(y_test,lr_random_pred_prob, 'Logistic Regression Random CV')
-ROC(y_test,rfc_random_pred_prob,'Random Forest Random Grid Search CV')
-ROC(y_test,gbc_random_pred_prob,'Gradient Boost Random Grid Search CV')
-ROC(y_test,dt_rand_pred_prob,'Decision Tree Random Grid Search CV')
-ROC(y_test,lda_rand_pred_prob,'LDA Random Grid Search CV')
-plt.legend()
-plt.show()
+# Bagging Classifier Randomized Grid Search
+BC_param_grid = {
+    'baggingclassifier__n_estimators': [50,100,200],
+    'baggingclassifier__max_samples': [1.0,0.8,0.6],
+    'baggingclassifier__max_features': [1.0,0.8,0.6],
+}
+
+BC_rand_search = RandomizedSearchCV(
+    make_pipeline(ct,BC),
+    BC_param_grid,
+    n_iter=10,
+    scoring='roc_auc',
+    cv=5,
+    random_state=42
+).fit(X_train,y_train)
+
+best_BC_estimator = BC_rand_search.best_estimator_
+BC_rand_pred = best_BC_estimator.predict(X_test)
+BC_rand_pred_prob = best_BC_estimator.predict_proba(X_test)[::,1]
+print(f'Bagging Classifier - Best Parameters: {BC_rand_search.best_params_}')
+print(f'Bagging Classifier - Best ROC-AUC Score: {BC_rand_search.best_score_ * 100:.2f}%')
