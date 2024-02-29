b094235254b62fdf9983e14353e416082efd67a7
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-02-29 14:02:00 -0500

    Update stroke_update.py

--- stroke_update.py
+++ stroke_update.py
@@ -16,10 +16,45 @@ df['id'].value_counts()
 
 df['stroke'].value_counts()
 
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+
+df = df.drop('id',axis=1)
+df1 = df.copy()
+
+for i in df1:
+    df1[i] = le.fit_transform(df1[i])
+    
+
+import seaborn as sns
+
+
+sns.heatmap(df1.corr(), annot=True)
+plt.show()
+
+
+def desc(df1):
+    _,axs = plt.subplots(2,2,figsize=(10,6))
+    sns.barplot(x='gender',y='stroke',ax=axs[0,0],data=df1)
+    sns.lineplot(x='age',y='smoking_status',ax=axs[0,1],data=df1)
+    sns.lineplot(x='bmi',y='age',ax=axs[1,0],data=df1)
+    sns.barplot(x='heart_disease',y='stroke',ax=axs[1,1],data=df1)
+    plt.show()
+    plt.tight_layout()
 
 
 
-X = df.drop(['id','stroke'],axis=1)
+desc(df1)
+
+
+
+
+
+
+
+
+X = df.drop('stroke',axis=1)
 y = df[['stroke']]
 
 
@@ -28,94 +63,87 @@ from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 
 
 
 
-clf = LogisticRegression()
 
-rfc = RandomForestClassifier()
-
-knn = KNeighborsClassifier(n_neighbors=3)
-
-nb = GaussianNB()
-
-GBC = GradientBoostingClassifier()
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.compose import make_column_transformer
 
 ohe = OneHotEncoder()
-imputer = SimpleImputer()
+imputer = SimpleImputer(strategy='mean')
+
 
 ct = make_column_transformer(
-    (ohe,['ever_married','work_type','Residence_type','smoking_status']),
-    (imputer,['bmi']),remainder='passthrough')
+    (ohe, X.select_dtypes(include='object').columns),
+    (imputer, X.select_dtypes(['int64', 'float64']).columns),remainder='passthrough')
+
+
 
 
 ct.fit_transform(X)
 
-
-
 from sklearn.pipeline import make_pipeline
 
-clf_pipe = make_pipeline(ct,clf).fit(X_train,y_train)
-clf_pred = clf_pipe.predict(X_test)
-clf_pred_prob = clf_pipe.predict_proba(X_test)[::,1]
+rfc = RandomForestClassifier()
+
+nb = GaussianNB()
+lr = LogisticRegression()
+
+knn = KNeighborsClassifier(n_neighbors=7)
+
+GBC = GradientBoostingClassifier()
+BC = BaggingClassifier()
 
 
-rfc_pipe = make_pipeline(ct,rfc).fit(X_train,y_train)
-rfc_pred = rfc_pipe.predict(X_test)
-rfc_pred_prob = rfc_pipe.predict_proba(X_test)[::,1]
-
-
-knn_pipe = make_pipeline(ct,knn).fit(X_train,y_train)
-knn_pred = knn_pipe.predict(X_test)
-knn_pred_prob = knn_pipe.predict_proba(X_test)[::,1]
-
-
-
-nb_pipe = make_pipeline(ct,nb).fit(X_train,y_train)
-nb_pred = nb_pipe.predict(X_test)
-nb_pred_prob = nb_pipe.predict_proba(X_test)[::,1]
-
-
-GBC_pipe = make_pipeline(ct,GBC).fit(X_train,y_train)
-GBC_pred = GBC_pipe.predict(X_test)
-GBC_pred_prob = GBC_pipe.predict_proba(X_test)[::,1]
+from sklearn.metrics import roc_auc_score,roc_curve,accuracy_score,f1_score,confusion_matrix
 
 
 
-from sklearn.metrics import roc_auc_score,roc_curve,accuracy_score
-
-def evaluate_model(model_name,y_true,y_pred,y_pred_prob):
-    acc = accuracy_score(y_true, y_pred)
-    roc = roc_auc_score(y_true, y_pred_prob)
-    print(f'{model_name} - Accuracy: {acc * 100:.2f}%, ROC-AUC: {roc * 100:.2f}%')
-
-evaluate_model('Logistic Regression', y_test,clf_pred,clf_pred_prob)
-evaluate_model('Random Forest', y_test,rfc_pred,rfc_pred_prob)
-evaluate_model('Naive Bayes', y_test,nb_pred,nb_pred_prob)
-evaluate_model('Gradient Boosting',y_test,GBC_pred,GBC_pred_prob)
-evaluate_model('KNN', y_test,knn_pred, knn_pred_prob)
-
+def evaluate_model(X_train,X_test,y_train,y_test,model):
+    pipe = make_pipeline(ct,model).fit(X_train,y_train)
+    pred = pipe.predict(X_test)
+    pred_prob = pipe.predict_proba(X_test)[:,1]
+    acc = accuracy_score(y_test, pred)
+    f1 = f1_score(y_test, pred)
+    con = confusion_matrix(y_test, pred)
+    roc = roc_auc_score(y_test,pred_prob)
+    print('confusion matrix: ',con)
+    print(f'{model.__class__.__name__}, --Accuracy Score-- {acc*100:.2f}%; --ROC-- {roc*100:.2f}%; --F1-- {f1*100:.2f}%')
+    return pred,pred_prob
 
 
-def roc_curve_plot(y_true, y_pred_prob, model_name):
-    fpr, tpr, _ = roc_curve(y_true, y_pred_prob)
-    plt.plot(fpr,tpr,label=model_name)
-    plt.title('ROC Curve')
+
+
+
+lr_pred,lr_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, lr)
+rfc_pred,rfc_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, rfc)
+nb_pred,nb_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, nb)
+BC_pred,BC_pred_prob = evaluate_model(X_train, X_test, y_train, y_test,BC)
+gbc_pred,gbc_pred_prob = evaluate_model(X_train, X_test, y_train, y_test,GBC)
+knn_pred,knn_pred_prob = evaluate_model(X_train, X_test, y_train, y_test,knn)
+
+
+
+def ROC_Curve(y_test,y_pred_prob,model):
+    fpr,tpr, _ = roc_curve(y_test, y_pred_prob)
+    plt.plot(fpr,tpr,label=model.__class__.__name__)
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
 
-roc_curve_plot(y_test,clf_pred_prob,'Logistic Regression')
-roc_curve_plot(y_test,rfc_pred_prob,'Random Forest')
-roc_curve_plot(y_test,knn_pred_prob,'KNN')
-roc_curve_plot(y_test,nb_pred_prob,'Naive Bayes')
-roc_curve_plot(y_test,GBC_pred_prob,'Gradient Boosting')
+
+
+ROC_Curve(y_test,lr_pred_prob,lr)
+ROC_Curve(y_test,rfc_pred_prob,rfc)
+ROC_Curve(y_test,gbc_pred_prob,GBC)
+ROC_Curve(y_test,nb_pred_prob,nb)
+ROC_Curve(y_test, knn_pred_prob,knn)
+ROC_Curve(y_test,BC_pred_prob,BC)
 plt.legend()
 plt.show()
