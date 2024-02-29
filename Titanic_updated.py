1c3acb014d8ac860405131fe110d295957d85668
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-02-29 12:38:21 -0500

    Update Titanic_updated.py

--- Titanic_updated.py
+++ Titanic_updated.py
@@ -65,13 +65,13 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.30,random_state=
 
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 
 
-clf = LogisticRegression()
+lr = LogisticRegression()
 
 rfc = RandomForestClassifier()
 
@@ -82,7 +82,7 @@ tree_clf = DecisionTreeClassifier()
 gb = GradientBoostingClassifier()
 
 nb = GaussianNB()
-
+BC = BaggingClassifier()
 
 
 from sklearn.preprocessing import OneHotEncoder
@@ -94,7 +94,9 @@ ohe = OneHotEncoder()
 imp = SimpleImputer(strategy='mean')
 
 
-ct = make_column_transformer((ohe,['Pclass','Embarked','Sex']),(imp,['Age']),remainder='passthrough')
+ct = make_column_transformer(
+    (ohe,X.select_dtypes(include='object').columns),
+    (imp,X.select_dtypes(include=['int64','float64']).columns),remainder='passthrough')
 
 
 
@@ -103,59 +105,44 @@ ct.fit_transform(X)
 
 from sklearn.pipeline import make_pipeline
 
-pipe_clf = make_pipeline(ct,clf).fit(X_train,y_train)
-pred_clf = pipe_clf.predict(X_test)
-clf_pred_prob = pipe_clf.predict_proba(X_test)[::,1]
-
-pipe_rfc = make_pipeline(ct,rfc).fit(X_train,y_train)
-pred_rfc = pipe_rfc.predict(X_test)
-rfc_pred_prob = pipe_rfc.predict_proba(X_test)[::,1]
-
-pipe_knn = make_pipeline(ct,knn).fit(X_train,y_train)
-pred_knn = pipe_knn.predict(X_test)
-knn_pred_prob = pipe_knn.predict_proba(X_test)[::,1]
-
-
-pipe_tree = make_pipeline(ct,tree_clf).fit(X_train,y_train)
-pred_tree = pipe_tree.predict(X_test)
-pred_prob_tree = pipe_tree.predict_proba(X_test)[::,1]
-
-
-pipe_gb = make_pipeline(ct,gb).fit(X_train,y_train)
-gb_pred = pipe_gb.predict(X_test)
-gb_pred_prob = pipe_gb.predict_proba(X_test)[::,1]
-
-nb_pipe = make_pipeline(ct,nb).fit(X_train,y_train)
-nb_pred = nb_pipe.predict(X_test)
-nb_pred_prob = nb_pipe.predict_proba(X_test)[::,1]
-
 
 from sklearn.metrics import accuracy_score,roc_auc_score,roc_curve
 
-def evaluate_model(model_name,y_true,y_pred,y_pred_prob):
-    acc = accuracy_score(y_true, y_pred)
-    roc = roc_auc_score(y_true, y_pred_prob)
-    print(f'{model_name} - Accuracy: {acc * 100:.2f}%, ROC-AUC: {roc * 100:.2f}%')
-
-evaluate_model('Logistic Regression', y_test,pred_clf,clf_pred_prob)
-evaluate_model('Random Forest', y_test,pred_rfc,rfc_pred_prob)
-evaluate_model('Naive Bayes', y_test,nb_pred,nb_pred_prob)
-evaluate_model('Gradient Boosting',y_test,gb_pred,gb_pred_prob)
-evaluate_model('KNN', y_test,pred_knn, knn_pred_prob)
+def evaluate_model(X_train,X_test,y_train,y_test,model):
+    pipe = make_pipeline(ct,model).fit(X_train,y_train)
+    pred = pipe.predict(X_test)
+    pred_prob = pipe.predict_proba(X_test)[:,1]
+    acc = accuracy_score(y_test,pred)
+    roc = roc_auc_score(y_test, pred_prob)
+    print(f'{model} - Accuracy: {acc * 100:.2f}%, ROC-AUC: {roc * 100:.2f}%')
+    return pred,pred_prob
 
 
 
-def roc_curve_plot(y_true, y_pred_prob, model_name):
+
+lr_pred,lr_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, lr)
+rfc_pred,rfc_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, rfc)
+knn_pred,knn_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, knn)
+gb_pred,gb_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, gb)
+nb_pred,nb_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, nb)
+BC_pred,BC_pred_prob = evaluate_model(X_train, X_test, y_train, y_test, BC)
+
+
+
+
+def roc_curve_plot(y_true, y_pred_prob, model):
     fpr, tpr, _ = roc_curve(y_true, y_pred_prob)
-    plt.plot(fpr,tpr,label=model_name)
+    plt.plot(fpr,tpr,label=model.__class__.__name__)
     plt.title('ROC Curve')
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
 
-roc_curve_plot(y_test,clf_pred_prob,'Logistic Regression')
-roc_curve_plot(y_test,rfc_pred_prob,'Random Forest')
-roc_curve_plot(y_test,knn_pred_prob,'KNN')
-roc_curve_plot(y_test,nb_pred_prob,'Naive Bayes')
-roc_curve_plot(y_test,gb_pred_prob,'Gradient Boosting')
+roc_curve_plot(y_test,lr_pred_prob,lr)
+roc_curve_plot(y_test,rfc_pred_prob,rfc)
+roc_curve_plot(y_test,knn_pred_prob,knn)
+roc_curve_plot(y_test,nb_pred_prob,nb)
+roc_curve_plot(y_test,gb_pred_prob,gb)
 plt.legend()
 plt.show()
+
+
