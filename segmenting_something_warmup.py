afaf2ba1ece85d620caca3e07a98ab6ab127101b
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-05-11 17:04:28 -0400

    Update segmenting_something_warmup.py

--- segmenting_something_warmup.py
+++ segmenting_something_warmup.py
@@ -35,36 +35,36 @@ plt.show()
 
 def desc(df1):
     _,axs = plt.subplots(2,2,figsize=(10,6))
-    sns.barplot(x='Category',y='Gender',ax=axs[0,0],data=df1)
+    sns.barplot(x='Category',y='Purchase_Amount_(USD)',ax=axs[0,0],data=df1)
     sns.lineplot(x='Age',y='Item_Purchased',ax=axs[0,1],data=df1)
     sns.lineplot(x='Purchase_Amount_(USD)',y='Age',ax=axs[1,0],data=df1)
-    sns.barplot(x='Season',y='Shipping_Type',ax=axs[1,1],data=df1)
+    sns.barplot(x='Season',y='Discount_Applied',ax=axs[1,1],data=df1)
     plt.show()
 
 
 desc(df1)
+
 
 df['Subscription_Status'] = [1 if X == 'Yes' else 0 for X in df['Subscription_Status']]
 
 X = df.drop('Subscription_Status',axis=1)
 y = df['Subscription_Status']
 
-
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
 
 X.dtypes
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.preprocessing import OneHotEncoder,MinMaxScaler
 
 
 ohe = OneHotEncoder(sparse_output=False)
-sc = StandardScaler()
+ms = MinMaxScaler()
 
 from sklearn.compose import make_column_transformer
 
 ct = make_column_transformer(
     (ohe,X.select_dtypes(include='object').columns),
-    (sc,X.select_dtypes(include=['float64','int64']).columns),remainder='passthrough')
+    (ms,X.select_dtypes(include=['float64','int64']).columns),remainder='passthrough')
 
 
 ct.fit_transform(X)
@@ -81,7 +81,7 @@ GBC = GradientBoostingClassifier()
 RFC = RandomForestClassifier()
 BC = BaggingClassifier()
 
-from sklearn.metrics import roc_auc_score,roc_curve,accuracy_score
+from sklearn.metrics import roc_auc_score,roc_curve,accuracy_score,f1_score,confusion_matrix
 
 
 def evaluate_model(X_train,X_test,y_train,y_test,model):
@@ -90,6 +90,10 @@ def evaluate_model(X_train,X_test,y_train,y_test,model):
     pred_prob = pipe.predict_proba(X_test)[:,1]
     acc = accuracy_score(y_test,pred)
     roc = roc_auc_score(y_test,pred_prob)
+    con = confusion_matrix(y_test,pred)
+    f1 = f1_score(y_test,pred)
+    print('f1 score: ',round(f1*100))
+    print('confusion matrix',con)
     print(f'{model.__class__.__name__}, --ACC-- {acc*100:.2f}%; --ROC-- {roc*100:.2f}%')
     return pred,pred_prob
 
@@ -110,9 +114,9 @@ def ROC(y_test,y_pred_prob,model):
 ROC(y_test,lr_pred_prob,lr)
 ROC(y_test,GBC_pred_prob,GBC)
 ROC(y_test,RFC_pred_prob,RFC)
+ROC(y_test,BC_pred_prob,BC)
 plt.legend()
 plt.show()
-
 
 
 
