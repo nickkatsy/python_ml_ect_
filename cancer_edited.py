f3e702ed0edb42ae236a0e769cd5ca046f88b84a
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-05-14 12:34:43 -0400

    Update cancer_edited.py

--- cancer_edited.py
+++ cancer_edited.py
@@ -1,32 +1,37 @@
 import pandas as pd
-import warnings
-warnings.filterwarnings('ignore')
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 df = pd.read_csv('https://raw.githubusercontent.com/nickkas/python_ml_ect_/master/cancer.csv')
 
+df.describe()
+df.isna().sum()
+df = df.drop('Unnamed: 32',axis=1)
+print(df.dtypes)
+df = df.drop('id',axis=1)
 
 df.info()
 
-df.isna().sum()
-
-df.nunique()
-
-print(df.dtypes)
-df.shape
-
-df = df.drop(['Unnamed: 32','id'],axis=1)
-
-df.corr()
 df['diagnosis'].value_counts()
 
+df['diagnosis'] = [1 if X == 'B' else 0 for X in df['diagnosis']]
 
-df['diagnosis'] = pd.get_dummies(df.diagnosis,prefix='diagnosis').iloc[:,0:1]
 
-def subplots(df):
-    his,ax1 = plt.subplots(4,3,figsize=(12,6))
-    sns.kdeplot(df,x='diagnosis',ax=ax1[0,0])
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def desc_stats(df):
+    f,axs = plt.subplots(2,2,figsize=(10,6))
+    sns.barplot(x='diagnosis',y='area_worst',ax=axs[0,0],data=df)
+    sns.scatterplot(x='perimeter_mean',y='fractal_dimension_se',ax=axs[0,1],data=df)
+    sns.scatterplot(x='texture_worst',y='texture_mean',ax=axs[1,0],data=df)
+    sns.scatterplot(x='concavity_mean',y='symmetry_mean',ax=axs[1,1],data=df)
+    plt.show()
+
+
+
+def hist(df):
+    his,ax1 = plt.subplots(4,3,figsize=(15,5))
+    sns.countplot(df,x='diagnosis',ax=ax1[0,0])
     sns.histplot(df,x='radius_mean',ax=ax1[0,1])
     sns.histplot(df,x='texture_mean',ax=ax1[0,2])
     sns.histplot(df,x='perimeter_mean',ax=ax1[1,0])
@@ -42,7 +47,6 @@ def subplots(df):
 
 
 
-subplots(df)
 
 
 
@@ -51,11 +55,12 @@ subplots(df)
 
 
 X = df.drop('diagnosis',axis=1)
-y = df[['diagnosis']]
+y = df['diagnosis']
 
-y.value_counts(normalize=True)
+import statsmodels.api as sm
 
-
+model_ = sm.GLM(exog=sm.add_constant(X),endog=y).fit()
+print(model_.summary())
 
 
 
@@ -63,54 +68,66 @@ from sklearn.model_selection import train_test_split
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
 
+from sklearn.preprocessing import MinMaxScaler
 
-from sklearn.preprocessing import StandardScaler
+ms = MinMaxScaler()
 
-sc = StandardScaler()
-X_train_scaled = sc.fit_transform(X_train)
-X_test_scaled = sc.transform(X_test)
+X_train_scaled = ms.fit_transform(X_train)
+X_test_scaled = ms.transform(X_test)
 
-
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,BaggingClassifier
+BC = BaggingClassifier()
+GBC = GradientBoostingClassifier()
+RFC = RandomForestClassifier()
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,BaggingClassifier
-from sklearn.naive_bayes import GaussianNB
-
-
 lr = LogisticRegression()
 
-rfc = RandomForestClassifier()
-gbr = GradientBoostingClassifier()
-NB = GaussianNB()
-BC = BaggingClassifier()
+
+from sklearn.metrics import roc_auc_score,roc_curve,accuracy_score,confusion_matrix,f1_score
 
 
-
-
-
-
-
-
-from sklearn.metrics import roc_auc_score,accuracy_score,confusion_matrix,f1_score
-
-
-
-def evaluate_model(X_train_scaled,X_test_scaled,y_train,y_test,model):
+def eval_models(X_train_scaled,X_test_scaled,y_train,y_test,model):
     model = model.fit(X_train_scaled,y_train)
     pred = model.predict(X_test_scaled)
     pred_prob = model.predict_proba(X_test_scaled)[:,1]
+    f1 = f1_score(y_test,pred)
     acc = accuracy_score(y_test, pred)
-    f1 = f1_score(y_test, pred)
-    con = confusion_matrix(y_test, pred)
-    roc = roc_auc_score(y_test, pred_prob)
-    print(f'{model.__class__.__name__}, --ACC-- {acc*100:.2f}%; --ROC-- {roc*100:.2f}%; --f1-- {f1*100:.2f}%')
+    roc = roc_auc_score(y_test,pred_prob)
+    con = confusion_matrix(y_test,pred)
     print('confusion matrix',con)
+    print('f1 score',f1*100)
+    print(f'{model.__class__.__name__}, --ACC-- {acc*100:.2f}%; --ROC-- {roc*100:.2f}%')
     return pred,pred_prob
 
+lr_pred,lr_pred_prob = eval_models(X_train_scaled, X_test_scaled, y_train, y_test,lr)
+GBC_pred,GBC_pred_prob = eval_models(X_train_scaled, X_test_scaled, y_train, y_test, GBC)
+BC_pred,BC_pred_prob = eval_models(X_train_scaled, X_test_scaled, y_train, y_test,BC)
+RFC_pred,RFC_pred_prob = eval_models(X_train_scaled, X_test_scaled, y_train, y_test, RFC)
 
-lr_pred,lr_pred_prob = evaluate_model(X_train_scaled, X_test_scaled, y_train, y_test, lr)
-rfc_pred,rfc_pred_prob = evaluate_model(X_train_scaled, X_test_scaled, y_train, y_test, rfc)
-NB_pred,NB_pred_prob = evaluate_model(X_train_scaled, X_test_scaled, y_train, y_test, NB)
-BC_pred,BC_pred_prob = evaluate_model(X_train, X_test, y_train, y_test,BC)
-gbr_pred,gbr_pred_prob = evaluate_model(X_train_scaled, X_test_scaled, y_train, y_test,gbr)
+
+def ROC(y_test,y_pred_prob,model):
+    fpr,tpr, _ = roc_curve(y_test,y_pred_prob)
+    plt.plot(fpr,tpr,label=model.__class__.__name__)
+    plt.title('Roc Curves of Models')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+
+
+
+
+def main():
+    desc_stats(df)
+    hist(df)
     
+    ROC(y_test,lr_pred_prob,lr)
+    ROC(y_test,GBC_pred_prob,GBC)
+    ROC(y_test,RFC_pred_prob,RFC)
+    ROC(y_test,BC_pred_prob,BC)
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
