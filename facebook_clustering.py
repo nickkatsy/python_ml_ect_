e9e9e7dafe24e9df4ec20b5ccc80086033952b3f
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-05-20 13:36:54 -0400

    Update facebook_clustering.py

--- facebook_clustering.py
+++ facebook_clustering.py
@@ -2,14 +2,11 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
+df = pd.read_csv('https://raw.githubusercontent.com/nickkatsy/python_ml_ect_/master/kmeans_fb.csv')
 
-data = pd.read_csv('https://raw.githubusercontent.com/nickkatsy/python_ml_ect_/master/kmeans_fb.csv')
-data.info()
-data.isna().sum()
-data = data.drop(['Column1','Column2','Column3','Column4','status_id','status_published'],axis=1)
+df = df.drop(['Column1','Column2','Column3','Column4','status_id','status_published'],axis=1)
 
-df = data.copy()
-df.info()
+
 
 
 from sklearn.preprocessing import LabelEncoder
@@ -28,14 +25,17 @@ ms = MinMaxScaler()
 
 df_scaled = ms.fit_transform(df)
 
+
+
+
+
 from sklearn.cluster import KMeans
 
-# storing iteration of 10 clusters iterated 10 times to find labels and intertia
+
 
 inertia = []
-for i in range(1,11):
-    kmeans = KMeans(n_clusters=i,init='k-means++',n_init=20,max_iter=10,random_state=0)
-    kmeans.fit(df_scaled)
+for i in range(1,12):
+    kmeans = KMeans(n_clusters=i,init='k-means++',n_init=20,max_iter=10,random_state=0).fit(df_scaled)
     inertia.append(kmeans.inertia_)
 
 kmeans.inertia_
@@ -45,29 +45,29 @@ kmeans.labels_
 #Elbow Method for optimal number of clusters
 
 import matplotlib.pyplot as plt
-plt.plot(range(1,11),inertia,marker='*')
+plt.plot(range(1,12),inertia,marker='*')
 plt.xlabel('Clusters')
 plt.ylabel('inertia')
 plt.title('Elbow Method')
 
-# Optimal Number of Clusters
-k = 4
+# Optimal Number of Clusters k = 3
 
-kmeans = KMeans(n_clusters=k,init='k-means++',n_init=20,max_iter=10,random_state=0)
-kmeans.fit(df_scaled)
+kmeans = KMeans(n_clusters=3,init='k-means++',n_init=20,max_iter=10,random_state=0).fit(df_scaled)
 labels = kmeans.labels_
-df['cluster'] = labels
+df['cluster 1'] = labels
+df['cluster 2'] = labels
 
-plt.scatter(df_scaled[:,0],df_scaled[:,1],c=df['cluster'],cmap='viridis',s=300,linewidths=10,edgecolors='r')
+
+plt.scatter(df_scaled[:,0],df_scaled[:,1],c=df['cluster 1'],cmap='viridis',s=300,linewidths=10,edgecolors='r')
+plt.scatter(df_scaled[:,0],df_scaled[:,1],c=df['cluster 2'],cmap='viridis',s=300,linewidths=10,edgecolors='m')
 plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],marker='x',s=300,linewidth=10,color='k')
 plt.show()
 
-print(kmeans.inertia_)
 
-# scoring the cluster at k = 4
+
+# scoring the cluster at k = 3
 from sklearn.metrics import silhouette_score
 
 SilhouetteScore = silhouette_score(df_scaled,labels)
 print(f'score: {SilhouetteScore*100:.2f}%')
-
-
+print(kmeans.inertia_)
