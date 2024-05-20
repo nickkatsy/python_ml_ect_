710b38b8118e4258a72118457b79cdcf9db4d283
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-05-20 13:23:52 -0400

    Update food_clustering.py

--- food_clustering.py
+++ food_clustering.py
@@ -1,28 +1,11 @@
 import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
-import matplotlib.pyplot as plt
+
 
 df = pd.read_csv('https://raw.githubusercontent.com/LeeMorinUCF/QMB6358S22/main/demo_25_Dimension_Reduction/protein.csv')
-
 df.info()
-
 df = df.drop('Country',axis=1)
-df.isna().sum()
-
-print(df.dtypes)
-df.drop_duplicates(inplace=True)
-
-df.describe()
-
-
-
-#heatmap to show correlations amound the features in the dataset
-
-import seaborn as sns
-
-sns.heatmap(df.corr(), annot=True)
-plt.show()
 
 
 from sklearn.preprocessing import MinMaxScaler
@@ -31,59 +14,47 @@ ms = MinMaxScaler()
 
 df_scaled = ms.fit_transform(df)
 
-
-
 from sklearn.decomposition import PCA
 
-
 pca = PCA(n_components=2)
-
 pca_scaled = pca.fit_transform(df_scaled)
-
-
 
 
 from sklearn.cluster import KMeans
 
 
-#appending iterations from 1-12 into a library titled 'cc' to to use the elbow method to find 
-# optimal cluster
-
 cc = []
 for i in range(1,12):
-    kmeans = KMeans(n_clusters=i,init='k-means++',n_init=20,random_state=42).fit(pca_scaled)
+    kmeans = KMeans(n_clusters=i,init='k-means++',n_init=20,random_state=1).fit(pca_scaled)
     cc.append(kmeans.inertia_)
 
 
-#The Elbow Method method to find optimal number of clusters
 
+import matplotlib.pyplot as plt
 
-plt.plot(range(1,12),cc,marker='*')
-plt.xlabel('Clusters')
-plt.ylabel('Inertia')
-plt.title('The Elbow Method')
+plt.plot(range(1,12),cc,marker='X')
 plt.show()
 
 
-# kmeans using optimal point from elbow method
-kmeans = KMeans(n_clusters=8,init='k-means++',n_init=20,random_state=42).fit(pca_scaled)
-labels = kmeans.predict(pca_scaled)
-df['Cluster'] = labels
+# k = 9
+
+kmeans = KMeans(n_clusters=9,init='k-means++',n_init=20,random_state=1).fit(pca_scaled)
+labels = kmeans.labels_
+df['Cluster 1'] = labels
+df['Cluster 2'] = labels
 
 
-# A scatterplot of k = 8
-plt.scatter(pca_scaled[:,0],pca_scaled[:,1],c=df['Cluster'],marker='x',edgecolors='r')
-plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],marker='*',color='m')
+plt.scatter(pca_scaled[:,0],pca_scaled[:,1],c=df['Cluster 1'],edgecolors='r',marker='X')
+plt.scatter(pca_scaled[:,0],pca_scaled[:,1],c=df['Cluster 2'],edgecolors='m',marker='*')
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],marker='x',edgecolors='s')
+
 plt.show()
 
 
-# scoring
 from sklearn.metrics import silhouette_score
 
 sh = silhouette_score(pca_scaled,labels)
 print(f'the sh score is: {sh*100:.2f}%')
-print(kmeans.inertia_)
-
-
+print('inertia: ',kmeans.inertia_)
 
 
