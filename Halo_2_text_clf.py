f5c66bbaaeb287762b9cb069b5bb829fe5a2996a
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-05-20 20:22:44 -0400

    Update Halo_2_text_clf.py

--- Halo_2_text_clf.py
+++ Halo_2_text_clf.py
@@ -1,74 +1,56 @@
 import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
-
-
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 df = pd.read_csv('https://raw.githubusercontent.com/nickkatsy/python_ml_ect_/master/arbiter.csv')
 
-df['name'] = df['name'].str.replace('W','')
-df['line'] = df['line'].str.replace('W','')
+nltk.download('punkt')
 
 
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-tfid = TfidfVectorizer()
+df['line'] = df['line'].str.replace('W',"")
+df['name'] = df['name'].str.replace("W","")
 
 
 X = df['line']
 y = df['name']
 
+
+
+def tokenizer_text(text):
+    return nltk.word_tokenize(text)
+
+
+tfid = TfidfVectorizer(tokenizer=tokenizer_text)
+
 from sklearn.model_selection import train_test_split
+
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
 
 
 X_train = tfid.fit_transform(X_train)
 X_test = tfid.transform(X_test)
 
-
-
-from sklearn.naive_bayes import MultinomialNB
-
-MNB = MultinomialNB()
-
-
-
-from sklearn.ensemble import GradientBoostingClassifier,BaggingClassifier
-
-GBC = GradientBoostingClassifier()
-BC = BaggingClassifier()
-
 from sklearn.linear_model import LogisticRegression
-lr = LogisticRegression()
+
+from sklearn.ensemble import BaggingClassifier,GradientBoostingClassifier
+
+from sklearn.svm import SVC
+
+models = {
+    "GBC":GradientBoostingClassifier(),
+    "LR":LogisticRegression(),
+    "BC":BaggingClassifier(),
+    "svc":SVC(probability=True)
+    }
 
 
 from sklearn.metrics import accuracy_score,classification_report
 
-
-def evaluate_model(X_train,X_test,y_train,y_test,model):
-    model = model.fit(X_train,y_train)
-    pred = model.predict(X_test)
-    acc = accuracy_score(y_test,pred)
-    clf_rpt = classification_report(y_test,pred)
-    print(f'{model.__class__.__name__}, --ACC-- {acc*100:.2f}%; -- CLF RPT -- {clf_rpt}')
-    return pred
-
-lr_pred = evaluate_model(X_train, X_test, y_train, y_test, lr)
-MNB_pred = evaluate_model(X_train, X_test, y_train, y_test, MNB)
-BC_pred = evaluate_model(X_train, X_test, y_train, y_test,BC)
-GBC_pred = evaluate_model(X_train, X_test, y_train, y_test, GBC)
-
-
-
-def get_lines_by_character(character_name,df):
-
-    lines_spoken = df[df['name'] == character_name]['line']
-    return lines_spoken
-
-
-unique_characters = df['name'].unique()
-for character in unique_characters:
-    lines_spoken = get_lines_by_character(character, df)
-    print(f"Lines spoken by {character}:")
-    print(lines_spoken)
+for name,models in models.items():
+    models.fit(X_train,y_train)
+    y_pred = models.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    clf_rpt = classification_report(y_test, y_pred)
+    print(f'{models} -- ACC -- {acc*100:.2f}%; --Classification Report-- {clf_rpt}')
