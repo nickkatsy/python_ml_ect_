257a5c82b650526abc49e993a2c5c5d129637150
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-05-23 12:04:01 -0400

    Update credit_speedrun.py

--- credit_speedrun.py
+++ credit_speedrun.py
@@ -4,19 +4,9 @@ warnings.filterwarnings('ignore')
 
 
 df = pd.read_csv('https://raw.githubusercontent.com/LeeMorinUCF/QMB6358S22/main/demo_24_Classification/credit.csv')
-
-
 df.info()
 df.isna().sum()
 print(df.dtypes)
-
-df.nunique()
-
-
-df['Default'] = [1 if X == 1 else 0 for X in df['Default']]
-
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
 df1 = df.copy()
@@ -28,21 +18,20 @@ le = LabelEncoder()
 for i in df1:
     df1[i] = le.fit_transform(df1[i])
 
-plt.figure(figsize=(12,6))
-sns.heatmap(df1.corr(), annot=True)
 
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def sub(df1):
-    plt_,axs = plt.subplots(2,2,figsize=(10,6))
-    sns.lineplot(x='duration',y='age',ax=axs[0,0],data=df1,hue='Default')
-    sns.countplot(x='Default',ax=axs[0,1],data=df1)
-    sns.barplot(x='savings',y='Default',ax=axs[1,0],data=df1)
-    sns.barplot(x='foreign',y='Default',ax=axs[1,1],data=df1)
+    f,axs = plt.subplots(2,2,figsize=(10,6))
+    sns.barplot(x='job',y='Default',ax=axs[0,0],data=df1)
+    sns.barplot(x='installment',y='Default',ax=axs[0,1],data=df1)
+    sns.scatterplot(x='amount',y='age',ax=axs[1,0],data=df1)
+    sns.barplot(x='employ',y='Default',ax=axs[1,1],data=df1)
     plt.show()
     
-
-
 sub(df1)
 
 
@@ -50,114 +39,108 @@ X = df.drop('Default',axis=1)
 y = df['Default']
 
 
-
 from sklearn.model_selection import train_test_split
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=0)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
 
-
-
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 ohe = OneHotEncoder()
-
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-
 
 from sklearn.compose import make_column_transformer
 
 ct = make_column_transformer(
-    (ohe,X.select_dtypes(include='object').columns),(sc,X.select_dtypes(include=['int64']).columns),remainder='passthrough')
+    (ohe,X.select_dtypes(include='object').columns),
+    (sc,X.select_dtypes(include=['int64','float64']).columns),remainder='passthrough')
 
 
-X_train_transformed = ct.fit_transform(X_train)
-X_test_transformed = ct.transform(X_test)
+
+ct.fit_transform(X)
 
 
+from sklearn.pipeline import make_pipeline
 
 from sklearn.linear_model import LogisticRegression
 lr = LogisticRegression()
 
 from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,BaggingClassifier
-
-rfc = RandomForestClassifier()
-gbc = GradientBoostingClassifier()
 BC = BaggingClassifier()
+RFC = RandomForestClassifier()
+GBC = GradientBoostingClassifier()
+
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+lda = LinearDiscriminantAnalysis()
 
 from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier()
 
-from sklearn.naive_bayes import GaussianNB
-nb = GaussianNB()
-
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=8)
+knn = KNeighborsClassifier(n_neighbors=11)
 
-from sklearn.pipeline import make_pipeline
-
-
+from sklearn.metrics import roc_auc_score,roc_curve,accuracy_score,f1_score,confusion_matrix
 
 
-
-from sklearn.metrics import roc_auc_score,roc_curve,f1_score,confusion_matrix,accuracy_score
-
-
-def evaluate_model(model,X_train_transformed,X_test_transformed,y_train,y_test):
+def evaluate(X_train,X_test,y_train,y_test,model):
     pipe = make_pipeline(ct,model).fit(X_train,y_train)
     pred = pipe.predict(X_test)
-    pred_prob = pipe.predict_proba(X_test)[::,1]
-    acc = accuracy_score(y_test, pred)
+    pred_prob = pipe.predict_proba(X_test)[:,1]
+    acc = accuracy_score(y_test,pred)
     roc = roc_auc_score(y_test, pred_prob)
-    f1 = f1_score(y_test, pred)
-    cm = confusion_matrix(y_test, pred)
-    print('Confusion Matrix',cm)
-    print(f'{model.__class__.__name__} --Accuracy-- {acc*100:.2f}%; --ROC-- {roc*100:.2f}%; --F1-- {f1*100:.2f}%')
+    con = confusion_matrix(y_test,pred)
+    f1 = f1_score(y_test,pred)
+    print('f1 score: ',f1)
+    print('confusion matrix',con)
+    print(f'{model.__class__.__name__}, --ACC-- {acc*100:.2f}%; --ROC-- {roc*100:.2f}%')
     return pred,pred_prob
 
-lr_pred,lr_pred_prob = evaluate_model(lr, X_train_transformed, X_test_transformed, y_train, y_test)
-rfc_pred,rfc_pred_prob = evaluate_model(rfc,X_test_transformed,X_test_transformed,y_train,y_test)
-gbc_pred,gbc_pred_prob =  evaluate_model(gbc, X_train_transformed, X_test_transformed, y_train, y_test)
-nb_pred,nb_pred_prob = evaluate_model(nb, X_train_transformed, X_test_transformed, y_train, y_test)
-knn_pred,knn_pred_prob = evaluate_model(knn, X_train_transformed, X_test_transformed, y_train, y_test)
-tree_pred,tree_pred_prob = evaluate_model(tree, X_train_transformed, X_test_transformed, y_train, y_test)
-BC_pred,BC_pred_prob = evaluate_model(BC, X_train_transformed, X_test_transformed, y_train, y_test)
+lr_pred,lr_pred_prob = evaluate(X_train, X_test, y_train, y_test, lr)
+GBC_pred,GBC_pred_prob = evaluate(X_train, X_test, y_train, y_test, GBC)
+tree_pred,tree_pred_prob = evaluate(X_train, X_test, y_train, y_test, tree)
+knn_pred,knn_pred_prob = evaluate(X_train, X_test, y_train, y_test, knn)
+lda_pred,lda_pred_prob = evaluate(X_train, X_test, y_train, y_test, lda)
+RFC_pred,RFC_pred_prob = evaluate(X_train, X_test, y_train, y_test, RFC)
+BC_pred,BC_pred_prob = evaluate(X_train, X_test, y_train, y_test, BC)
 
 
-def ROC_Curve(y_test,y_pred_prob,model):
-    fpr,tpr, _ = roc_curve(y_test, y_pred_prob)
+
+def ROC(y_test,y_pred_prob,model):
+    fpr,tpr, _ = roc_curve(y_test,y_pred_prob)
     plt.plot(fpr,tpr,label=model.__class__.__name__)
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
+    plt.title('ROC Curves')
+    
 
-
-
-ROC_Curve(y_test,lr_pred_prob,lr)
-ROC_Curve(y_test,rfc_pred_prob,rfc)
-ROC_Curve(y_test,gbc_pred_prob,gbc)
-ROC_Curve(y_test,tree_pred_prob,tree)
-ROC_Curve(y_test,nb_pred_prob,nb)
-ROC_Curve(y_test, knn_pred_prob,knn)
-ROC_Curve(y_test,BC_pred_prob,BC)
+ROC(y_test, lr_pred_prob, lr)
+ROC(y_test,GBC_pred_prob, GBC)
+ROC(y_test,RFC_pred_prob,RFC)
+ROC(y_test,tree_pred_prob,tree)
+ROC(y_test,lda_pred_prob,lda)
+ROC(y_test, knn_pred_prob,knn)
+ROC(y_test, BC_pred_prob, BC)
 plt.legend()
 plt.show()
 
 
-# Cross-Validation
+
 
 from sklearn.model_selection import cross_val_score
 
 
-def cross_validation(model,X_train_transformed,y_train):
-    model = model.fit(X_train_transformed,y_train)
-    cv_score = cross_val_score(model, X_train_transformed,y_train,cv=5,scoring='roc_auc').mean()
-    print(f'{model.__class__.__name__}, --5-fold Cross-Validation Score-- {cv_score*100:.2f}%')
-    return cv_score
-    
+def cross_validation(X,y,model):
+    pipe = make_pipeline(ct,model).fit(X,y)
+    cv_scores = cross_val_score(pipe, X,y,cv=10,scoring='roc_auc').max()
+    print(f'{model.__class__.__name__}, The cv scores using 10-fold-cross validation: {cv_scores*100:.2f}')
+    return cv_scores
 
-lr_score = cross_validation(lr, X_train_transformed, y_train)
-gbc_score = cross_validation(gbc, X_train_transformed, y_train)
-rfc_score = cross_validation(rfc, X_train_transformed, y_train)
-tree_score = cross_validation(tree, X_train_transformed, y_train)
-nb_score = cross_validation(nb, X_train_transformed, y_train)
-knn_score = cross_validation(knn, X_train_transformed, y_train)
-BC_score = cross_validation(BC, X_train_transformed, y_train)
+
+lr_scores = cross_validation(X,y,lr)
+GBC_scores = cross_validation(X, y, GBC)
+RFC_scores = cross_validation(X, y, RFC)
+knn_scores = cross_validation(X, y, knn)
+tree_scores = cross_validation(X, y, tree)
+lda_scores = cross_validation(X, y, lda)
+BC_scores = cross_validation(X, y, BC)
+
+
