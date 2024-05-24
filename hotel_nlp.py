835541a6afab00fd895d40fbb69ba5affac4008f
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2024-05-24 15:24:38 -0400

    Update hotel_nlp.py

--- hotel_nlp.py
+++ hotel_nlp.py
@@ -5,7 +5,7 @@ import nltk
 nltk.download('stopwords')
 
 
-df = pd.read_csv('C:/ML/python/data/Capsule Hotel Risk Classification Dataset (English Translated) - Sheet1.csv',delimiter=',')
+df = pd.read_csv('https://raw.githubusercontent.com/nickkatsy/python_ml_ect_/master/Capsule%20Hotel%20Risk%20Classification%20Dataset%20(English%20Translated)%20-%20Sheet1.csv')
 
 nltk.download('punkt')
 df.info()
@@ -38,6 +38,30 @@ def remove_punc(text):
 
 df['content'] = df['content'].apply(remove_punc)
 df['translated_content'] = df['translated_content'].apply(remove_punc)
+
+
+
+def remove_emoji(text):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  
+                           u"\U0001F300-\U0001F5FF"  
+                           u"\U0001F680-\U0001F6FF"  
+                           u"\U0001F1E0-\U0001F1FF"  
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+
+
+df['content'] = df['content'].apply(remove_emoji)
+df['translated_content'] = df['translated_content'].apply(remove_emoji)
+
+
+
+
+
+
 
 from nltk.tokenize import word_tokenize
 
@@ -110,7 +134,7 @@ from sklearn.model_selection import cross_val_score
 
 def cross_(X,y,model):
     model = model.fit(X,y)
-    cv_scores = cross_val_score(model, X,y,cv=10,scoring='accuracy').max()
+    cv_scores = cross_val_score(model, X,y,cv=10,scoring='accuracy').mean()
     print(f'{model.__class__.__name__}, --Cross Validation Scores -- {cv_scores*100:.2f}%')
     return cv_scores
 
@@ -118,6 +142,5 @@ RFC_scores = cross_(X, y, RFC)
 GBC_scores = cross_(X,y,GBC)
 tree_scores = cross_(X,y,tree)
 lr_scores = cross_(X, y, lr)
-
 
 
